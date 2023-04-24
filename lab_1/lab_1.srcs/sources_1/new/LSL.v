b0d307c2764595f5270eeea524de14acee8607b4
gtgilchrist <77506773+gtgilchrist@users.noreply.github.com>
2023-04-24 14:31:14 -0700

    Update LSL.v

--- lab_1/lab_1.srcs/sources_1/new/LSL.v
+++ lab_1/lab_1.srcs/sources_1/new/LSL.v
@@ -1,20 +1,24 @@
 `timescale 1ns / 1ps
-module ASL(
+module LSL(
   input [15:0] A, 
   input [15:0] B,
-  output [15:0] A_shifted,
-  output [1:0] overflow);
-  
-A_shifted = A;
-  
-For(int j=0;j<B;j++){
-  For(int i=15;i<=1;i--)
-    A_shifted[i] = A_shifted[i-1];
-  A_shifted[0] = 0;
-}
-  
-if(A_shifted[15] != A[15])
-    overflow = 1'b1;
-else
-    overflow = 1'b0;
+  output reg [15:0] A_shifted,
+  output reg [1:0] overflow);
+  integer i;
+  integer j;
+
+  always @(A,B)
+    begin 
+      
+  	  A_shifted = A;
+      overflow = 1'b0;
+  		
+      for(j=0; j<B; j=j+1)begin 
+        for(i=15; i>=1; i=i-1) 
+    	  A_shifted[i] = A_shifted[i-1];
+  		 A_shifted[0] = 0;
+        
+       end
+    end
+      
 endmodule 
