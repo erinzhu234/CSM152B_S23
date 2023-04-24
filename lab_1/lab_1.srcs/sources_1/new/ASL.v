ab9d31c8fe6eea863ddfc8576e35386a62ffdcab
gtgilchrist <77506773+gtgilchrist@users.noreply.github.com>
2023-04-24 14:30:36 -0700

    Update ASL.v

--- lab_1/lab_1.srcs/sources_1/new/ASL.v
+++ lab_1/lab_1.srcs/sources_1/new/ASL.v
@@ -2,19 +2,27 @@
 module ASL(
   input [15:0] A, 
   input [15:0] B,
-  output [15:0] A_shifted,
-  output [1:0] overflow);
-  
-A_shifted = A;
-  
-For(int j=0;j<B;j++){
-  For(int i=15;i<=1;i--)
-    A_shifted[i] = A_shifted[i-1];
-  A_shifted[0] = 0;
-}
+  output reg [15:0] A_shifted,
+  output reg [1:0] overflow);
+  integer i;
+  integer j;
  
-if(A_shifted[15] != A[15])
-    overflow = 1'b1;
-else
-    overflow = 1'b0;
+  always @(A,B)
+    begin
+ 
+      A_shifted = A;
+      
+      for(j=0; j<B; j=j+1)begin
+        for(i=15; i>=1; i=i-1)
+             A_shifted[i] = A_shifted[i-1];
+  		  A_shifted[0] = 0;
+      end
+   
+      if(A_shifted[15] != A[15]) 
+        overflow = 1'b1;
+	  else 
+ 	  	overflow = 1'b0;
+      
+	end
+  
 endmodule 
