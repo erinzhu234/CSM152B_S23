70bf78a220f2b09087acd3affffe1ebbf94aab66
erinzhu234 <102999671+erinzhu234@users.noreply.github.com>
2023-05-01 12:44:22 -0700

    Update alu16_tb.v

--- lab_1/lab_1.srcs/sim_1/new/alu16_tb.v
+++ lab_1/lab_1.srcs/sim_1/new/alu16_tb.v
@@ -133,6 +133,45 @@ module alu16_tb(
         a=16'b1000000000000000;
         b=16'b0000000000000001;
         #10
+
+        // testbench for register file
+        rst = 1;
+        #10
+        rst = 0;
+        #10
+        ra = 5'b01101;
+        #10
+        $display("reg file is now %h", busA);
+        #10
+        rb = 5'b11010;
+        rw = 5'b11010;
+        busW = 16'h3e6f;
+        wren = 1;
+        #10
+        ra = 5'b00110;
+        rw = 5'b00110;
+        busW = 16'h9ac4;
+        wren = 1;
+        #20
+        $display("00110 is now %h", busA);
+        $display("11010 is now %h", busB);
+        #10
+        wren = 0;
+        ra = 5'b01000;
+        rw = 5'b01000;
+        busW = 16'h000f;
+        #20
+        $display("01000 is now %h", busA);
+        #10
+        rst = 1;
+        #10
+        rst = 0;
+        #10
+        ra = 5'b00110;
+        rb = 5'b11010;
+        #30
+        $display("00110 is now %h", busA);
+        $display("11010 is now %h", busB);
         $stop;
     end
 
