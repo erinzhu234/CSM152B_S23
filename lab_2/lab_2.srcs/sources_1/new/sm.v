f7f984cacef897b375bbeca7cfa4675cbd6d0265
erinzhu234 <102999671+erinzhu234@users.noreply.github.com>
2023-05-03 13:37:44 -0700

    lab 2 set up

---
+++ lab_2/lab_2.srcs/sources_1/new/sm.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/03/2023 01:24:03 PM
+// Design Name: 
+// Module Name: sm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sm(
+    input clk, sensor, ped,
+    output [2:0] main, [2:0] side, lamp
+    );
+    
+endmodule
