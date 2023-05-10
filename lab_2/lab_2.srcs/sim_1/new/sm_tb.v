90305bf3c54a5ee8901291949c1bac323c7e4433
erinzhu234 <102999671+erinzhu234@users.noreply.github.com>
2023-05-10 12:29:57 -0700

    .

---
+++ lab_2/lab_2.srcs/sim_1/new/sm_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/10/2023 12:19:48 PM
+// Design Name: 
+// Module Name: sm_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sm_tb(
+
+    );
+    reg clk;
+    reg sensor;
+    reg ped;
+    reg rst;
+    wire mg;
+    wire my;
+    wire mr;
+    wire sg;
+    wire sy;
+    wire sr;
+    wire walk;
+    always
+        clk = #1 ~clk;
+    sm uut(clk, sensor, ped, rst, mg,my,mr, sg,sy,sr,walk);
+    initial begin
+        clk=1;
+        sensor=0;
+        ped=0;
+        rst=0;
+        #10
+        $stop;
+    end
+endmodule
