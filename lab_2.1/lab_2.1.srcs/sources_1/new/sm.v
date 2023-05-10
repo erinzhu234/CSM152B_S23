1d695072beaa1c8ae2d571cf5f70e5214b8c0a58
erinzhu234 <102999671+erinzhu234@users.noreply.github.com>
2023-05-10 12:33:09 -0700

    new file

---
+++ lab_2.1/lab_2.1.srcs/sources_1/new/sm.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/10/2023 12:32:45 PM
+// Design Name: 
+// Module Name: sm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sm(
+
+    );
+endmodule
