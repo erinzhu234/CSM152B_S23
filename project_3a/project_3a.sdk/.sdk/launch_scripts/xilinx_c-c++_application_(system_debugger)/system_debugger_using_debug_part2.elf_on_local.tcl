5cb067bc669e06f142a2b1aa8fa7b595333602d3
erinzhu234 <102999671+erinzhu234@users.noreply.github.com>
2023-05-17 13:55:13 -0700

    started lab3

---
+++ project_3a/project_3a.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_part2.elf_on_local.tcl
@@ -0,0 +1,8 @@
+connect -url tcp:127.0.0.1:3121
+configparams mdm-detect-bscan-mask 2
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31C2FA"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31C2FA"} -index 0
+dow C:/Users/Student/Documents/GitHub/CSM152B_S23/project_3a/project_3a.sdk/part2/Debug/part2.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31C2FA"} -index 0
+con
