ee76fc80d279cae5aefc97ac61fdf63eaf93a791
gtgilchrist <77506773+gtgilchrist@users.noreply.github.com>
2023-05-17 12:42:38 -0700

    Update sm_tb.v

--- lab_2.1/lab_2.1.srcs/sim_1/new/sm_tb.v
+++ lab_2.1/lab_2.1.srcs/sim_1/new/sm_tb.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/10/2023 12:37:36 PM
-// Design Name: 
-// Module Name: sm_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module sm_tb(
 
     );
