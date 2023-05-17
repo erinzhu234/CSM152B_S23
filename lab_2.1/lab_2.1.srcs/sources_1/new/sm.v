6a14e06531e86915d71e9fc3a4ade396bf269d5a
gtgilchrist <77506773+gtgilchrist@users.noreply.github.com>
2023-05-17 12:42:21 -0700

    Update sm.v

--- lab_2.1/lab_2.1.srcs/sources_1/new/sm.v
+++ lab_2.1/lab_2.1.srcs/sources_1/new/sm.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/10/2023 12:32:45 PM
-// Design Name: 
-// Module Name: sm
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module sm(
     input clk, sensor, ped, rst,
     output reg mg,my,mr, sg,sy,sr,walk,
