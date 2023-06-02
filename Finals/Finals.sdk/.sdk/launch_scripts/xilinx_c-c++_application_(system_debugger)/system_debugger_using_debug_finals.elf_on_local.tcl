bbf5735c76ff63fcbf40eb34e812d9c2ccef0ef8
erinzhu234 <102999671+erinzhu234@users.noreply.github.com>
2023-06-02 15:58:10 -0700

    ..

---
+++ Finals/Finals.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_finals.elf_on_local.tcl
@@ -0,0 +1,8 @@
+connect -url tcp:127.0.0.1:3121
+configparams mdm-detect-bscan-mask 2
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31C2FA"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31C2FA"} -index 0
+dow C:/Users/Student/Documents/GitHub/CSM152B_S23/Finals/Finals.sdk/finals/Debug/finals.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31C2FA"} -index 0
+con
